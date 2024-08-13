afe6a85161192df0dfca661839f87557720a917f
Landony Alvarado <laguilar@tribalworldwide.gt>
2024-08-13 14:33:26 -0600

    * Delete button back action.

--- app/src/main/java/softspark/com/inventorypilot/home/presentation/ui/home/HomeActivity.kt
+++ app/src/main/java/softspark/com/inventorypilot/home/presentation/ui/home/HomeActivity.kt
@@ -1,7 +1,6 @@
 package softspark.com.inventorypilot.home.presentation.ui.home
 
 import android.os.Bundle
-import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
@@ -20,7 +19,6 @@ class HomeActivity : AppCompatActivity() {
         setContentView(homeBinding.root)
 
         initBottomNavBar()
-        initListener()
     }
 
     private fun initBottomNavBar() {
@@ -30,11 +28,5 @@ class HomeActivity : AppCompatActivity() {
 
         // Configurar el BottomNavigationView con NavController
         NavigationUI.setupWithNavController(homeBinding.bottomNavigation, navController)
-    }
-
-    private fun initListener() {
-        onBackPressedDispatcher.addCallback(this) {
-            finish()
-        }
     }
 }
