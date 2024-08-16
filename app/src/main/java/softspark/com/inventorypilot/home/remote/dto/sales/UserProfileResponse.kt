b312a9ccaf631eef760ed8e3f370d21370302b88
Landony04 <dlandony123@gmail.com>
2024-08-16 10:57:43 -0600

    Delete response

--- app/src/main/java/softspark/com/inventorypilot/home/remote/dto/sales/UserProfileResponse.kt
+++
@@ -1,5 +0,0 @@
-package softspark.com.inventorypilot.home.remote.dto.sales
-
-import softspark.com.inventorypilot.login.remote.dto.UserProfileDto
-
-typealias UserProfileResponse = Map<String, UserProfileDto>
