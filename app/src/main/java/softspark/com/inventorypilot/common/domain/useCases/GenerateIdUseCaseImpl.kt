8ddbb90381b76b91272ac72c32b0905c4d963a36
Landony Alvarado <laguilar@tribalworldwide.gt>
2024-11-15 15:01:56 -0600

    * Generate id

--- app/src/main/java/softspark/com/inventorypilot/common/domain/useCases/GenerateIdUseCaseImpl.kt
+++ app/src/main/java/softspark/com/inventorypilot/common/domain/useCases/GenerateIdUseCaseImpl.kt
@@ -3,6 +3,10 @@ package softspark.com.inventorypilot.common.domain.useCases
 import java.util.UUID
 
 class GenerateIdUseCaseImpl : GenerateIdUseCase {
-    override fun invoke(): String = UUID.randomUUID()
-        .toString()
+
+    override fun invoke(): String {
+        val uuid = UUID.randomUUID().toString().replace("-", "")
+        val timestamp = System.currentTimeMillis().toString()
+        return "${uuid}_$timestamp" //
+    }
 }
